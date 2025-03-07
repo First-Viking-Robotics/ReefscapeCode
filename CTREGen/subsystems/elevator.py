87ae7afefe309db62ed978a66a9fa59735a4c5ac
Alexander Doyle <57422635+Suave101@users.noreply.github.com>
2025-03-06 18:17:23 -0600

    Update elevator.py

--- CTREGen/subsystems/elevator.py
+++ CTREGen/subsystems/elevator.py
@@ -3,74 +3,103 @@ import math
 import commands2
 import rev
 import wpilib
+import wpilib.shuffleboard
 import wpimath.controller
 import wpimath.estimator
 import wpimath.units
 import wpimath.trajectory
 import wpimath.system
 import wpimath.system.plant
-import phoenix6
-import numpy as np
+import constants
 
 
 class Elevator(commands2.Subsystem):
-    def __init__(self):
+    def __init__(self, constants=constants.Constants()):
         super().__init__()
+        self.constants = constants
+        self.profile = wpimath.trajectory.TrapezoidProfile(
+            wpimath.trajectory.TrapezoidProfile.Constraints(
+                self.constants.ElevatorMaxVelocity,
+                self.constants.ElevatorMaxAccel,  # Max elevator speed and acceleration.
+            )
+        )
 
-        self.motorFirst = rev.CANSparkMax(self.constants.ElevatorFirstMotorPort, rev.CANSparkMax.MotorType.kBrushless)
-        self.motorSecond = rev.CANSparkMax(self.constants.ElevatorSecondMotorPort, rev.CANSparkMax.MotorType.kBrushless)
+        self.PIDController = wpimath.controller.ProfiledPIDController(
+            0.6, 0, 0, wpimath.trajectory.TrapezoidProfile.Constraints(
+                self.constants.ElevatorMaxVelocity,
+                self.constants.ElevatorMaxAccel
+            )
+        )
 
-        # A joystick to read the trigger from.
-        self.joystick = wpilib.Joystick(self.constants.ElevatorJoystickPort)
+        self.motorFirst = rev.SparkMax(self.constants.ElevatorFirstMotorPort, rev.SparkMax.MotorType.kBrushless)
+        self.motorSecond = rev.SparkMax(self.constants.ElevatorSecondMotorPort, rev.SparkMax.MotorType.kBrushless)
+        self.motorFirst.setVoltage(12)
+        self.motorSecond.setVoltage(12)
 
-        # Circumference = pi * d, so distance per click = pi * d / counts
-        self.distancePerRotation = math.tau * self.constants.ElevatorDrumRadius
+        # An encoder set up to measure flywheel velocity in radians per second.
+        self.encoder = self.motorFirst.getAlternateEncoder()
+
+        self.PIDController.reset(
+            measuredPosition=self.encoder.getPosition(),
+            measuredVelocity=self.encoder.getVelocity()
+        )
 
         # Preset Goal
         self.goal = self.constants.ElevatorRest
 
+        # Disable Motors at Start
+        self.disable()
+        self.encoder.setPosition(0)
+
+        wpilib.SmartDashboard.putNumber("Elevator Goal", 0)
+
     def initMovement(self) -> None:
-        # Reset our loop to make sure it's in a known state.
-        self.loop.reset(np.array([
-            (self.encoder.get_position().value * self.distancePerRotation),
-            (self.encoder.get_velocity().value * self.distancePerRotation)
-        ]))
 
         # Reset our last reference to the current state.
         self.lastProfiledReference = wpimath.trajectory.TrapezoidProfile.State(
-            (self.encoder.get_position().value * self.distancePerRotation),
-            (self.encoder.get_velocity().value * self.distancePerRotation)
+            self.encoder.getPosition(),
+            wpimath.units.rotationsPerMinuteToRadiansPerSecond(self.encoder.getVelocity())
         )
 
     def goToL1(self):
-        return ElevatorToL1(self)
+        return commands2.cmd.runOnce(
+            lambda: self._goToL1(), self
+        )
 
     def goToL2(self):
-        return ElevatorToL2(self)
+        return commands2.cmd.runOnce(
+            lambda: self._goToL2(), self
+        )
 
     def goToL3(self):
-        return ElevatorToL3(self)
+        return commands2.cmd.runOnce(
+            lambda: self._goToL3(), self
+        )
 
     def goToL4(self):
-        return ElevatorToL4(self)
+        return commands2.cmd.runOnce(
+            lambda: self._goToL4(), self
+        )
 
     def goToRest(self):
-        return ElevatorToRest(self)
+        return commands2.cmd.runOnce(
+            lambda: self._goToRest(), self
+        )
 
     def _goToL1(self):
-        self.goal = self.constants.ElevatorL1
+        self.goal = 1
 
     def _goToL2(self):
-        self.goal = self.constants.ElevatorL2
+        self.goal = 2
 
     def _goToL3(self):
-        self.goal = self.constants.ElevatorL3
+        self.goal = 3
 
     def _goToL4(self):
-        self.goal = self.constants.ElevatorL4
+        self.goal = 4
 
     def _goToRest(self):
-        self.goal = self.constants.ElevatorRest
+        self.goal = 0
 
     def disable(self):
         self.motorSecond.setVoltage(0)
@@ -80,89 +109,17 @@ class Elevator(commands2.Subsystem):
         # Sets the target position of our arm. This is similar to setting the setpoint of a
         # PID controller.
 
-        goal = wpimath.trajectory.TrapezoidProfile.State(self.goal, 0.0)
-
-        # Step our TrapezoidalProfile forward 20ms and set it as our next reference
-        self.lastProfiledReference = self.profile.calculate(
-            0.020, self.lastProfiledReference, goal
-        )
-        self.loop.setNextR(
-            np.array([self.lastProfiledReference.position, self.lastProfiledReference.velocity])
+        percentageOutput = self.PIDController.calculate(
+            self.encoder.getPosition(),
+            wpilib.SmartDashboard.getNumber("Elevator Goal", 0)
         )
 
-        # Correct our Kalman filter's state vector estimate with encoder data.
-        self.loop.correct(np.array([(self.encoder.get_position().value * self.distancePerRotation)]))
+        wpilib.SmartDashboard.putNumber("Elevator Position", self.encoder.getPosition())
+        wpilib.SmartDashboard.putNumber("Elevator PowerOutput", (percentageOutput + 0.1) * 0.75)
 
-        # Update our LQR to generate new voltage commands and use the voltages to predict the next
-        # state with out Kalman filter.
-        self.loop.predict(0.020)
-
-        # Send the new calculated voltage to the motors.
-        # voltage = duty cycle * battery voltage, so
-        # duty cycle = voltage / battery voltage
-        nextVoltage = self.loop.U(0)
-        self.motorFirst.setVoltage(nextVoltage)
-        self.motorSecond.setVoltage(nextVoltage)
+        # wpimath.filter.SlewRateLimiter(3)
+        self.motorFirst.getClosedLoopController().setReference((percentageOutput + 0.1) * -0.75, rev.SparkMax.ControlType.kDutyCycle)
+        self.motorSecond.getClosedLoopController().setReference((percentageOutput + 0.1) * -0.75, rev.SparkMax.ControlType.kDutyCycle)
 
     def atGoal(self):
-        return ((self.encoder.get_position().value * self.distancePerRotation) == self.goal) and ((self.encoder.get_velocity().value * self.distancePerRotation) == 0)
-
-
-class ElevatorToL1(commands2.Command):
-    def __init__(self, elevator: Elevator):
-        super().__init__()
-        self._sub = elevator
-        self.addRequirements(self._sub)
-
-    def execute(self):
-        self._sub._goToL1()
-        if self._sub.atGoal():
-            self.isFinished()
-
-
-class ElevatorToL2(commands2.Command):
-    def __init__(self, elevator: Elevator):
-        super().__init__()
-        self._sub = elevator
-        self.addRequirements(self._sub)
-
-    def execute(self):
-        self._sub._goToL2()
-        if self._sub.atGoal():
-            self.isFinished()
-
-
-class ElevatorToL3(commands2.Command):
-    def __init__(self, elevator: Elevator):
-        super().__init__()
-        self._sub = elevator
-        self.addRequirements(self._sub)
-
-    def execute(self):
-        self._sub._goToL3()
-        if self._sub.atGoal():
-            self.isFinished()
-
-
-class ElevatorToL4(commands2.Command):
-    def __init__(self, elevator: Elevator):
-        super().__init__()
-        self._sub = elevator
-        self.addRequirements(self._sub)
-
-    def execute(self):
-        self._sub._goToL4()
-        if self._sub.atGoal():
-            self.isFinished()
-
-
-class ElevatorToRest(commands2.Command):
-    def __init__(self, elevator: Elevator):
-        super().__init__()
-        self._sub = elevator
-        self.addRequirements(self._sub)
-
-    def execute(self):
-        self._sub._goToRest()
-        if self._sub.atGoal():
-            self.isFinished()
+        return (self.encoder.getPosition() == self.goal) and (self.encoder.getVelocity() == 0)
